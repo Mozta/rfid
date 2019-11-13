b67c90aba3ee14cf0ead096ee199b003a5db5012
Rafael Luna <rafaelunaa@hotmail.com>
2019-11-13 15:02:11 -0600

    Added crud application and added it to settings and urls

---
+++ rfid_ui/crud/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def home(request):
+	return HttpResponse()
+
+def user(request):
+	return HttpResponse()
+
+def machine(request):
+	return HttpResponse()
