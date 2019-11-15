8c8bd5eb1397b79dbdf22c31e80564d0144ea80a
Rafael Luna <rafaelunaa@hotmail.com>
2019-11-15 13:01:15 -0600

    Got data from POST and sent it to dynamoDB

--- rfid_ui/crud/views.py
+++ rfid_ui/crud/views.py
@@ -1,11 +1,42 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+import os
+import boto3
+
+USERS_TABLE_NAME = os.environ['USERS_TABLE_NAME']
+MACHINES_TABLE_NAME = os.environ['MACHINES_TABLE_NAME']
 
 def home(request):
 	return render(request, 'crud/home.html')
 
 def user(request):
+
+	if request.method == 'POST':
+		dynamodb = boto3.resource('dynamodb')
+		users_table = dynamodb.Table(USERS_TABLE_NAME)
+		users_table.put_item(
+			Item = {
+			'authorized': True,
+			'id': request.POST.get('id'),
+			'name': request.POST.get('name'),
+			})
+		return redirect('crud-home')
+
 	return render(request, 'crud/user.html')
 
 def machine(request):
+
+	if request.method == 'POST':
+		dynamodb = boto3.resource('dynamodb')
+		users_table = dynamodb.Table(MACHINES_TABLE_NAME)
+		users_table.put_item(
+			Item = {
+			'description': request.POST.get('desc'),
+			'id': request.POST.get('id'),
+			'name': request.POST.get('name'),
+			'available': True,
+			'operator_id': ' '
+			})
+		return redirect('crud-home')
+
 	return render(request, 'crud/machine.html')
